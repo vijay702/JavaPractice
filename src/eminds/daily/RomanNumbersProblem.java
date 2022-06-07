e1d0adf7c25d8fc8251c91f3280900fa5356b30a
Vijay <vijayvijays763193@gmail.com>
2022-06-07 20:52:41 +0530

    after romanNumber Sum COmmit

---
+++ src/eminds/daily/RomanNumbersProblem.java
@@ -0,0 +1,58 @@
+package eminds.daily;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RomanNumbersProblem {
+
+    public static void main(String[] args) {
+
+        /*
+  I          1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000*/
+
+    String s ;
+
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" enter a String");
+    s =sc.next();
+    romanToInteger(s);
+    
+
+    }
+
+
+    public static void romanToInteger(String s){
+
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        map.put('I' , 1);
+        map.put('V' , 5);
+        map.put('X' , 10);
+        map.put('L' , 50);
+        map.put('C' , 100);
+        map.put('D' , 500);
+        map.put('M' , 1000);
+
+        int result =0;
+
+        for(int i=0; i<s.length();i++){
+
+
+            result =result+map.get(s.charAt(i));
+
+        }
+
+        System.out.println(result);
+
+
+    }
+}
