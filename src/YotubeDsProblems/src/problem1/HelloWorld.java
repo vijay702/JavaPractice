67a819ec24d1e1602dc59cce1312a5b2e6976fdf
Vijay <vijayvijays763193@gmail.com>
2022-06-15 12:48:46 +0530

    birthday commit

---
+++ src/YotubeDsProblems/src/problem1/HelloWorld.java
@@ -0,0 +1,31 @@
+package YotubeDsProblems.src.problem1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        printHello();
+
+    }
+
+
+    static void printHello(){
+        System.out.println("Hello World");
+        printHello2();
+    }
+
+
+
+    static void printHello2(){
+        System.out.println("Hello World");
+        printHello3();
+    }
+
+
+
+    static void printHello3(){
+        System.out.println("Hello World");
+    }
+
+
+}
